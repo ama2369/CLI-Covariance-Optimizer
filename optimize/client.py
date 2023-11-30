4285fdfd4a57f781b1982651346990b145594984
Mike A <ama2369@columbia.edu>
2023-11-29 23:01:09 -0500

    View ticker info

--- optimize/client.py
+++ optimize/client.py
@@ -1,8 +1,19 @@
 import click
+import yfinance as yf
 
-@click.command()
-def optimize():
-    click.echo("Hello World")
+@click.group()
+def cli():
+    """Usage: <ticker> to view data for stock"""
+    pass
+
+@cli.command(help='<ticker symbol> <start_date> <end_date>. Date format: <YYYY-MM-DD>. View stock data for a given ticker and date range')
+@click.argument('ticker_symbol')
+@click.argument('start_date')
+@click.argument('end_date')
+def ticker(ticker_symbol, start_date, end_date):
+    ticker_data = yf.Ticker(ticker_symbol)
+    historical_data = ticker_data.history(start=start_date, end=end_date)
+    click.echo(historical_data)
 
 if __name__ == '__main__':
-    optimize()
+    cli()
