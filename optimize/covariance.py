6b5b13b33687cfefdbeb28053d1772812d5f8c69
Mike A <ama2369@columbia.edu>
2023-12-11 21:31:16 -0500

    Implemented optimizing portfolio weights functionality

--- optimize/covariance.py
+++ optimize/covariance.py
@@ -1,21 +1,28 @@
-import pandas as pd
-import yfinance as yf
+import numpy as np
+from scipy.optimize import minimize
 
 def calculate_covariance(stock_data):
-    """
-    Calculate the covariance matrix of stock returns.
-
-    Parameters:
-    - stock_data (pandas DataFrame): DataFrame of stock prices with dates as index and tickers as columns.
-
-    Returns:
-    - pandas DataFrame: Covariance matrix of the stock returns.
-    """
-    # Calculate daily returns
     returns = stock_data.pct_change().dropna()
-
-    # Calculate the covariance matrix
     covariance_matrix = returns.cov()
-
     return covariance_matrix
 
+#Find the optimal asset weights for a portfolio, minimizing its variance
+def optimize_weights(matrix):
+    n = len(matrix)
+
+    #The sum of the weights must equal 1
+    constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+
+    #Each weight is between 0 and 1
+    bounds = tuple((0, 1) for asset in range(n))
+
+    #Calculate the portfolio variance
+    def portfolio_variance(weights):
+        return np.dot(weights.T, np.dot(matrix, weights))
+    
+    init_guess = np.full(n, 1/n)
+
+    #Minimize the portfolio_variance function 
+    opt_results = minimize(portfolio_variance, init_guess, method='SLSQP', bounds=bounds, constraints=constraints)
+
+    return opt_results.x
